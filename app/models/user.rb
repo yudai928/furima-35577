649daa19efa92317f294da1345e4b4bafc34d9f0
yudai928 <82750792+yudai928@users.noreply.github.com>
2021-05-14 14:39:07 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,7 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
-  
+
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX 
 end
