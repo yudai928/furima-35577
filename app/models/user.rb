4b106b7e1534cbd9dd7449abf5bfbaa51cf97951
yudai928 <82750792+yudai928@users.noreply.github.com>
2021-05-16 18:52:55 +0900

    revise1

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   end
 
   with_options presence: true,
-               format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください' } do
+               format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'に全角文字を使用してください' } do
     validates :last_name_kanji
     validates :first_name_kanji
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+   context '新規登録できるとき'do
+    it 'すべての情報が正しく入力されている場合、登録できること' do
+      expect(@user).to be_valid
+    end
+  end
+   context '新規登録できないとき' do
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
@@ -34,7 +40,8 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Password can't be blank")
     end
     it 'passwordは6文字以上でないと登録できない' do
-      @user.password = 'aaa'
+      @user.password = '11aaa'
+      @user.password_confirmation = '11aaa'
       @user.valid?
       expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
@@ -103,5 +110,6 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Birthday can't be blank")
     end
+  end
   end
 end
