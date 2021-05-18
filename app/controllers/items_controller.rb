baf305a5eac8a781d5f1932ee656eb660d05b5e0
yudai928 <82750792+yudai928@users.noreply.github.com>
2021-05-18 17:47:24 +0900

    コントローラー / 商品情報の取得

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @items = Item.all
   end
 
   def new
@@ -20,8 +21,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :price, :description, :image, :category_id, :condition_id, :shipping_rate_id, :shipping_area_id, :shipping_day_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :price, :description, :image, :category_id, :condition_id, :shipping_rate_id,
+                                 :shipping_area_id, :shipping_day_id).merge(user_id: current_user.id)
   end
 end
-
-
