74fea99db5593bfaa6c52b3981785cc0965adbe9
yudai928 <82750792+yudai928@users.noreply.github.com>
2021-05-21 15:49:35 +0900

    Pay.jpとJSを用いたクレジットカードの決済機能の実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,43 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :move_to_index
+
+  def index
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user.id || @item.order.present?
+  end
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :shipping_area_id, :city, :house_number, :building_name, :telephone_number).merge(
+      user_id: current_user.id, item_id: @item.id, token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+end
